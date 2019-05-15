2904144059f7b595cb18def8e3ec85854ae001b1
AdrienVictor <46904762+AdrienVictor@users.noreply.github.com>
2019-05-15 10:59:43 +0300

    adding login page on adrien branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import LogIn from './components/auth/LogIn';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <h1>
             Let us get the ball rolling guys as usual{' '}
-            <span role="img" aria-label="thumbs up">
+            <span role='img' aria-label='thumbs up'>
               👍
             </span>
           </h1>
         </header>
       </div>
+      <LogIn />
     </BrowserRouter>
   );
 }
