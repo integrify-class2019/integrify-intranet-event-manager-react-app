3d30ba6b84d584c48984ba002c493b840631fa2d
Sulaymon333 <tjsulaymon@gmail.com>
2019-05-25 21:30:39 +0300

    markup and ccss clean up

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,6 +2,8 @@
 
 const menuBar = document.querySelector('.menu-bar');
 const nav = document.querySelector('nav');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
 console.log(nav);
 
 const showMenu = () => {
