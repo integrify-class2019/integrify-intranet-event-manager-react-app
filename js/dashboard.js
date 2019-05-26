a2bbb15361db92e119574520d061ea26252e1837
Sulaymon333 <tjsulaymon@gmail.com>
2019-05-26 11:17:37 +0300

    js cleanup

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,12 +2,8 @@
 
 const menuBar = document.querySelector('.menu-bar');
 const nav = document.querySelector('nav');
-const header = document.querySelector('header');
-const main = document.querySelector('main');
-console.log(nav);
 
 const showMenu = () => {
-  console.log('clicked');
   nav.classList.toggle('open');
 };
 menuBar.addEventListener('click', showMenu);
