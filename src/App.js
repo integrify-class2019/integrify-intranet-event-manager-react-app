8f776d0cdc0dac0771ccde95e51c01bf5dec1d32
Sulaymon333 <tjsulaymon@gmail.com>
2019-05-27 18:16:33 +0300

    app-menu open and close when page is selected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import DashBoard from './components/dashboard/Dashboard';
 import LogIn from './components/auth/LogIn';
@@ -7,21 +7,35 @@ import CreateEvent from './components/events/CreateEvent';
 import './Base.css';
 import EventDetail from './components/dashboard/EventDetail';
 import Home from './components/Home';
+import NavBar from './components/layout/Navbar';
 
-const App = props => (
-    <BrowserRouter>
+class App extends Component {
+  state = {
+    isOpen: false
+  };
+
+  handleOpen = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
+
+  render() {
+    // console.log(this.state);
+    return (
+      <BrowserRouter>
         <div className="App">
-            <div className="mobile-wrapper">
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/dashboard" component={DashBoard} />
-                    <Route exact path="/sign-in" component={LogIn} />
-                    <Route exact path="/create-event" component={CreateEvent} />
-                    <Route path="/event/:id" component={EventDetail} />
-                </Switch>
-            </div>
+          <div className="mobile-wrapper">
+            <NavBar pageName="fixAfter" isOpen={this.state.isOpen} handleOpen={this.handleOpen} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/dashboard" component={DashBoard} />
+              <Route exact path="/sign-in" component={LogIn} />
+              <Route exact path="/create-event" component={CreateEvent} />
+              <Route path="/event/:id" component={EventDetail} />
+            </Switch>
+          </div>
         </div>
-    </BrowserRouter>
-);
-
+      </BrowserRouter>
+    );
+  }
+}
 export default App;
